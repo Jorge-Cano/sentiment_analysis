5461dc4e26e8638afbb7252ca1284afd938f7285
JAC-Keen <jorge.cano@keen.io>
2020-04-06 17:10:04 -0500

    script update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# https://redpanthers.co/get-into-sentiment-analysis-with-ruby/
+
 # sentiment_analysis
+# machine learning gems
+# try Tensor Flow next ..
+# https://github.com/somaticio/tensorflow.rb
---
+++ ml_sentiment.rb
@@ -0,0 +1,16 @@
+require "sentimentalizer"
+
+Sentimentalizer.setup
+
+class Analyzer
+  def initialize
+    Sentimentalizer.setup
+  end
+
+  sentiment = Sentimentalizer.analyze('I think Ruby is a great first language', true)
+  puts sentiment
+
+  sentiment_two = Sentimentalizer.analyze('Today is a struggle', true)
+  puts sentiment_two
+
+end
