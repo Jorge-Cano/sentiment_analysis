34e16070f3ca786433c36622ecd7bf50ae1f225c
JAC-Keen <jorge.cano@keen.io>
2020-04-06 16:58:46 -0500

    fixed inputs

--- sentiment_analysis.rb
+++ sentiment_analysis.rb
@@ -1,1 +1,13 @@
-gem install sentimental
+require "sentimental"
+analyzer = Sentimental.new
+analyzer.load_defaults
+puts "Please tell us about yourself, are you happy, are you kind, are you feeling bright?"
+phrase = gets.chomp
+puts phrase
+sentiment = analyzer.sentiment phrase
+score = analyzer.score phrase
+puts sentiment, score
+
+# sentiment = analyzer.sentiment 'Be the reason someone smiles today'
+# score = analyzer.score 'Be the reason someone smiles today'
+# puts sentiment, score
