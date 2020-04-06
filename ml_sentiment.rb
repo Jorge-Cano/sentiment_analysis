36bdb364c516c09d660d67af23efce1b401fed1b
JAC-Keen <jorge.cano@keen.io>
2020-04-06 18:45:37 -0500

    ml script inputs

--- ml_sentiment.rb
+++ ml_sentiment.rb
@@ -13,4 +13,9 @@ class Analyzer
   sentiment_two = Sentimentalizer.analyze('Today is a struggle', true)
   puts sentiment_two
 
+  puts "hello, how are you?"
+  user_input = gets.chomp
+  sentiment_three = Sentimentalizer.analyze(user_input, true)
+  puts(sentiment_three)
+
 end
